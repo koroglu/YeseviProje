c43a2f4971ffb7d57c1d5b28818eec5e9d0dbf18
koroglu <umitkilic@gmail.com>
2015-11-12 12:57:50 +0200

    Paketde degisiklikler

--- stajtakip-mvc/src/main/java/view/DenemeController.java
+++ stajtakip-mvc/src/main/java/view/DenemeController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 import dao.BaseDAO;
 
 @RestController
-public class SoaController  {
+public class DenemeController  {
 
 	@Autowired
 	private BaseDAO baseDAO;
